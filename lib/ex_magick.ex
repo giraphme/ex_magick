a4784c5d6ae1c50f357d42b8f9866a85b3c93ef2
TamakiMaeda <giraph.me@gmail.com>
2018-02-06 17:35:18 +0900

    Tweak the documentation

--- lib/ex_magick.ex
+++ lib/ex_magick.ex
@@ -2,27 +2,39 @@ defmodule ExMagick do
   alias ExMagick.CommandBuilder, as: Builder
 
   @doc """
-  ## Example
-    iex> ExMagick.init()
-    %ExMagick.CommandBuilder{command: "convert", options: [], output_name: nil}
+  Generate the command builder struct.
+
+  ## Examples
+
+      iex> ExMagick.init()
+      %ExMagick.CommandBuilder{command: "convert", options: []}
+
   """
   def init do
     %Builder{}
   end
 
   @doc """
-  ## Example
-    iex> ExMagick.init() |> ExMagick.open("test.png")
-    %ExMagick.CommandBuilder{command: "convert", options: ["test.png"], output_name: nil}
+  Set a file path to the command builder.
+
+  ## Examples
+
+      iex> ExMagick.init() |> ExMagick.open("test.png")
+      %ExMagick.CommandBuilder{command: "convert", options: ["test.png"]}
+
   """
   def open(%Builder{} = builder, path) when is_binary(path) do
     Builder.put_file(builder, path)
   end
 
   @doc """
-  ## Example
-    iex> ExMagick.init() |> ExMagick.open_with_base64("data:image/png;base64,....")
-    %ExMagick.CommandBuilder{command: "convert", options: ["inline:data:data:image/png;base64,...."], output_name: nil}
+  Set a file by base64 encoded image to the command builder.
+
+  ## Examples
+
+      iex> ExMagick.init() |> ExMagick.open_with_base64("data:image/png;base64,....")
+      %ExMagick.CommandBuilder{command: "convert", options: ["inline:data:data:image/png;base64,...."]}
+
   """
   def open_with_base64(%Builder{} = builder, encoded_image) do
     # TODO sanitize encoded_image
@@ -34,11 +46,15 @@ defmodule ExMagick do
     gradient: :gradient
   ]
   @doc """
-  ## Example
-    iex> ExMagick.init() |> ExMagick.open_with_color("#00aaff")
-    %ExMagick.CommandBuilder{command: "convert", options: ["xc:#00aaff"], output_name: nil}
-    iex> ExMagick.init() |> ExMagick.open_with_color("#00aaff-#ffaa00", fill: :gradient)
-    %ExMagick.CommandBuilder{command: "convert", options: ["gradient:#00aaff-#ffaa00"], output_name: nil}
+  Set a file by RGB color to the command builder.
+
+  ## Examples
+
+      iex> ExMagick.init() |> ExMagick.open_with_color("#00aaff")
+      %ExMagick.CommandBuilder{command: "convert", options: ["xc:#00aaff"]}
+      iex> ExMagick.init() |> ExMagick.open_with_color("#00aaff-#ffaa00", fill: :gradient)
+      %ExMagick.CommandBuilder{command: "convert", options: ["gradient:#00aaff-#ffaa00"]}
+
   """
   def open_with_color(%Builder{} = builder, rgb, opts \\ [fill: :fill]) do
     # TODO sanitize rgb to have # prefix
@@ -47,14 +63,21 @@ defmodule ExMagick do
   end
 
   @doc """
-  ## Example
-    iex> ExMagick.init() |> ExMagick.put_option("size", "150 x 150")
-    %ExMagick.CommandBuilder{command: "convert", options: ["-size 150 x 150"], output_name: nil}
+  Set an option to the command builder.
+
+  ## Examples
+
+      iex> ExMagick.init() |> ExMagick.put_option("size", "150x150")
+      %ExMagick.CommandBuilder{command: "convert", options: ["-size", "150x150"]}
+
   """
   def put_option(%Builder{} = builder, key, value) do
     Builder.put_option(builder, key, value)
   end
 
+  @doc """
+  Output the image from command builder.
+  """
   def output(%Builder{} = builder, output_name) do
     builder = Builder.put_file(builder, output_name)
     System.cmd(builder.command, builder.options)
